6c3a7ab4d55035b111527b7767e7010caf1add64
austinjp <austin.plunkett@gmail.com>
2025-09-05 18:35:18 +0100

    Use envsubst and padoc

--- scripts/html.sh
+++ scripts/html.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 
 errs=0
 for cmd in envsubst pandoc minify ; do
-	command -v "${cmd}" &>/dev/null || {
-		echo "Cannot find ${cmd}." >&2
-		errs=$(($errs+1))
-	}
+    command -v "${cmd}" &>/dev/null || {
+        echo "Cannot find ${cmd}." >&2
+        errs=$(($errs+1))
+    }
 done
 
 if [[ "${errs}" -gt 0 ]]; then
-	echo "Quitting!"
-	exit 1
+    echo "Quitting!"
+    exit 1
 fi
 
 
@@ -21,6 +21,8 @@ wd=$(readlink -qe "${this_dir}"/../)
 
 cd "${wd}"
 
-export BODY_HTML=$(pandoc sota.md)
+export BODY_HTML=$(
+    pandoc --strip-comments --wrap none --filter pandoc-crossref --citeproc --bibliography literature.bibtex sota.md
+)
 
 envsubst < index.raw | minify --type html --html-keep-document-tags --html-keep-end-tags > index.html
